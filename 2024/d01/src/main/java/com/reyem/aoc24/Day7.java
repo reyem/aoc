218dd0dd27810bd1c7b5cb87797c8781ef39c49b
Marco Meyer <marco.g.meyer@gmail.com>
2024-12-09 23:01:28 +0100

    solved day7

--- 2024/d01/src/main/java/com/reyem/aoc24/Day7.java
+++ 2024/d01/src/main/java/com/reyem/aoc24/Day7.java
@@ -11,8 +11,32 @@ public class Day7 {
 
     private static final String PUZZLE_INPUT = "day7/input.txt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        long puzzle1 = solvePuzzle1(PUZZLE_INPUT);
+        System.out.println("Puzzle 1 is: " + puzzle1);
 
+        long puzzle2 = solvePuzzle2(PUZZLE_INPUT);
+        System.out.println("Puzzle 2 is: " + puzzle2);
+    }
+
+    public static long solvePuzzle1(String input) throws IOException {
+        BufferedReader reader = getPuzzleInput(input);
+        List<Equation> equations = parseEquations(reader);
+        long result = 0;
+        for(Equation eq : equations) {
+            if(eq.couldBeTrue(Day7.Equation.OPERATORS_1)) result += eq.result;
+        }
+        return result;
+    }
+
+    public static long solvePuzzle2(String input) throws IOException {
+        BufferedReader reader = getPuzzleInput(input);
+        List<Equation> equations = parseEquations(reader);
+        long result = 0;
+        for(Equation eq : equations) {
+            if(eq.couldBeTrue(Day7.Equation.OPERATORS_2)) result += eq.result;
+        }
+        return result;
     }
 
     public static BufferedReader getPuzzleInput(String file) {
@@ -24,7 +48,7 @@ public class Day7 {
         String line;
         while((line = reader.readLine()) != null) {
             String[] parts = line.split(":");
-            int eqResult = Integer.parseInt(parts[0]);
+            long eqResult = Long.parseLong(parts[0]);
             String[] rightParts = parts[1].trim().split(" ");
             int[] numberParts = Arrays.stream(rightParts).mapToInt(Integer::parseInt).toArray();
             Equation eq = new Equation(eqResult, numberParts);
@@ -34,47 +58,70 @@ public class Day7 {
     }
 
     public static class Equation{
-        public int result;
+        public long result;
 
         public int[] parts;
 
-        public static final char[] operators = new char[]{'*', '+'};
+        public static final char[] OPERATORS_1 = new char[]{'*', '+'};
+        public static final char[] OPERATORS_2 = new char[]{'*', '+', '|'};
 
-        public Equation(int result, int[] parts) {
+        public Equation(long result, int[] parts) {
             this.result = result;
             this.parts = parts;
         }
 
-        public boolean couldBeTrue(int[] values) {
-               char[] equations = new char[(int)Math.pow(operators.length, values.length - 1)];
-               for(int i = 0; i < equations.length -1; i++) {
-                   equations[i]
-               }
+        public boolean couldBeTrue(char[] operators) {
+            ArrayList<ArrayList<Character>> allCombinations = calcAllPositions(operators, parts.length -1);
+            for(ArrayList<Character> eq : allCombinations) {
+                long left = parts[0];
+                for(int i = 1; i < parts.length; i++) {
+                    char op = eq.get(i-1);
+                    int right = parts[i];
+                    if(op == '*') {
+                        left *= right;
+                    }else if(op == '+'){
+                        left += right;
+                    }else if(op == '|') {
+                        left = Long.parseLong(String.valueOf(left) + String.valueOf(right));
+                    }
+                }
+                if(left == result) return true;
+            }
+            return false;
         }
 
-        public int[] calcAllResults() {
-            int[] results = new int[(int)Math.pow(operators.length, parts.length - 1)];
-            for(int i = 0; i< results.length; i++) {
-                // ddd
+        public static ArrayList<ArrayList<Character>> calcAllPositions(char[] operators, int n) {
+            int base = operators.length;
+            int k = (int) Math.pow(base, n); // binary 0 - k
+            ArrayList<ArrayList<Character>> result = new ArrayList<>();
+            for(int i = 0; i < k; i++) {
+                //0 == +
+                //1 == -
+                int s = i;
+                ArrayList<Character> comb = new ArrayList<>();
+                for(int p = 0; p < n; p++) {// TODO operators shift if not base 2??
+
+                    int r = s % base;
+                    s = s / base;
+                    if(r == 0) {
+                        comb.add('+');
+                    }else if(r == 1) {
+                        comb.add('*');
+                    }else if(r == 2) {
+                        comb.add('|');
+                    }
+                    /*if((s & 0x1) == 1) {
+                        comb.add('*');
+                    }else {
+                        comb.add('+');
+                    }
+                    s = s >> 1;*/
+                }
+                result.add(comb);
             }
-            return results;
+            return result;
         }
 
-        public int calcAdd(int[] values){
-            if(values.length == 2) {
-                return values[0] + values[1];
-            }else {
-                return values[0] + calcAdd(Arrays.copyOfRange(values, 1, values.length));
-            }
-        }
-
-        public int calcMultiply(int[] values){
-            if(values.length == 2) {
-                return values[0] * values[1];
-            }else {
-                return values[0] * calcMultiply(Arrays.copyOfRange(values, 1, values.length));
-            }
-        }
     }
 
 }
--- 2024/d01/src/test/java/com/reyem/aoc24/Day7Test.java
+++ 2024/d01/src/test/java/com/reyem/aoc24/Day7Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -26,6 +27,46 @@ class Day7Test {
         assertEquals(40, equationList.get(1).parts[1]);
         assertEquals(27, equationList.get(1).parts[2]);
 
+    }
+    @Test
+     void testPuzzle1() throws Exception{
+        long result = Day7.solvePuzzle1("day7/testinput.txt");
+        assertEquals(3749, result);
+    }
+
+    @Test
+    void testPuzzle2() throws Exception{
+        long result = Day7.solvePuzzle2("day7/testinput.txt");
+        assertEquals(11387, result);
+    }
+
+    @Test
+    void couldBeTrue() throws IOException{
+        BufferedReader reader = Day7.getPuzzleInput("day7/testinput.txt");
+        List<Day7.Equation> equationList = Day7.parseEquations(reader);
+
+        assertTrue(equationList.get(0).couldBeTrue(Day7.Equation.OPERATORS_1));
+        assertTrue(equationList.get(1).couldBeTrue(Day7.Equation.OPERATORS_1));
+        assertFalse(equationList.get(2).couldBeTrue(Day7.Equation.OPERATORS_1));
+        assertFalse(equationList.get(3).couldBeTrue(Day7.Equation.OPERATORS_1));
+
+
+    }
+
+    @Test
+    void calcPermutations() {
+        System.out.println(" base 2");
+
+        ArrayList<ArrayList<Character>> result = Day7.Equation.calcAllPositions(Day7.Equation.OPERATORS_1, 4);
+        for(ArrayList<Character> r : result) {
+            System.out.println(r.toString());
+        }
+
+        System.out.println(" base 3");
+        result = Day7.Equation.calcAllPositions(Day7.Equation.OPERATORS_2, 4);
+        for(ArrayList<Character> r : result) {
+            System.out.println(r.toString());
+        }
     }
 
 }
